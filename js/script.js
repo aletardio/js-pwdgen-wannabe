71253221bcdc9f47621a85a39723de53655a4f1b
aletardio <ale.tardio99@gmail.com>
2023-11-17 15:59:28 +0100

    Fixed position script in html

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ let surname = prompt('Inserisci il tuo cognome');
 
 let color = prompt('Inserisci il tuo colore preferito');
 
-let demo = `${name} ${surname} ${color} 23`;
+let demo = `${name}${surname}${color}23`;
 
 document.getElementById('subtitle').innerHTML = demo;
 
