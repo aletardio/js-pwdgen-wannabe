6271a2eb6d3aa6b6541b865805e10523135f908b
aletardio <ale.tardio99@gmail.com>
2023-11-17 15:33:57 +0100

    Added h2 on index and script

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let name = prompt('Inserisci il tuo nome');
+
+let surname = prompt('Inserisci il tuo cognome');
+
+let color = prompt('Inserisci il tuo colore preferito');
+
+let demo = `${name} ${surname} ${color} 23`;
+
+document.getElementById('subtitle').innerHTML = demo;
+
+console.log(demo);
